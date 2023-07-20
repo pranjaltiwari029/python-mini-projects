f1db39e46498387c40a5161581734b1ae4c5b151
Pranjal Tiwari <pranjaltiwari029@gmail.com>
2023-07-20 14:24:32 +0530

    send message using python

---
+++ whatsapp_message/message.py
@@ -0,0 +1,8 @@
+import pywhatkit
+
+phone_number="+917982319695"
+message="hello pranjal tiwari, hope u will be doing great"
+time_hour=00
+time_min=1
+# pywhatkit.sendwhatmsg(phone_number,message,time_hour,time_min)
+pywhatkit.sendwhatmsg_instantly(phone_number,message)
